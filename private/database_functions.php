27e40c1e028bc788c0bac502bae88c4c3cf24b39
dragan002 <draganvujic29@gmail.com>
2023-11-18 13:51:43 +0100

    Contection to Database - OOP

---
+++ private/database_functions.php
@@ -0,0 +1,16 @@
+<?php 
+
+function db_connect() {
+    $connection = new mysqli("DB_SERVER", "DB_USER","DB_PASS", "DB_NAME");
+    confirm_db_connect($connection);
+    return $connection;
+}
+
+function confirm_db_connect($connection) {
+    if($connection->connect_errno) {
+        $msg = "Database failed connection";
+        $msg .= $connection->connect_error;
+        $msg .= " (" . $connection->connect_errno . ")";
+        exit($msg);
+    }
+}
--- private/initialize.php
+++ private/initialize.php
@@ -24,6 +24,8 @@
   define("WWW_ROOT", $doc_root);
 
   require_once('functions.php');
+  require_once('db_credentials.php');
+  require_once('database_functions.php');
 
   // Load class definitions manually
 
@@ -42,5 +44,7 @@
     }
   }
   spl_autoload_register('my_autoload');
+
+  $database = db_connect();
 
 ?>
